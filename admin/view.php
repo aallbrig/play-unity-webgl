b3121d69adf6cb2453fd47848c1f50589b028733
Andrew Allbright <aallbrig@gmail.com>
2021-03-27 14:01:25 -0400

    Add WebGL Games custom post type

--- admin/view.php
+++ admin/view.php
@@ -3,9 +3,11 @@
 function play_unity_webgl_page_html()
 {
   ?>
-  <div class="wrap">
-    <h1><?php echo esc_html(get_admin_page_title()); ?> FOOBAR</h1>
-  </div>
+    <div class="wrap">
+        <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+        <form action="<?php menu_page_url('unity-webgl-games') ?>" method="post">
+            <button type="submit">Submit</button>
+        </form>
+    </div>
   <?php
 }
-
--- play-unity-webgl.php
+++ play-unity-webgl.php
@@ -10,16 +10,59 @@
 
 include "admin/view.php";
 
-// Top-Level Menus (https://developer.wordpress.org/plugins/administration-menus/top-level-menus/)
-add_action('admin_menu', 'unity_webgl_games_page');
-
-function unity_webgl_games_page()
+function unity_webgl_games_admin_page_submit()
 {
-  add_menu_page(
+  // TODO: Handle admin page form submit
+}
+
+function unity_webgl_games_admin_page()
+{
+  $hookname = add_menu_page(
     'Unity WebGL Games',
     'Unity WebGL Games',
     'manage_options',
     'unity-webgl-games',
     'play_unity_webgl_page_html'
-    );
+  );
+
+  add_action('load-' . $hookname, 'unity_webgl_games_admin_page_submit');
 }
+
+function unity_webgl_games_setup_post_types()
+{
+  register_post_type('unity_webgl_game', [
+    'labels' => [
+      'name' => __('WebGL Games', 'textdomain'),
+      'singular_name' => __('Game', 'textdomain')
+    ],
+    'public' => true,
+    'has_archive' => true,
+  ]);
+}
+
+function unity_webgl_games_activate()
+{
+  unity_webgl_games_setup_post_types();
+
+  flush_rewrite_rules();
+}
+
+function unity_webgl_games_deactivate()
+{
+  unregister_post_type('game');
+
+  flush_rewrite_rules();
+}
+
+
+function main()
+{
+  register_activation_hook(__FILE__, 'unity_webgl_games_activate');
+  register_deactivation_hook(__FILE__, 'unity_webgl_games_deactivate');
+
+  add_action('init', 'unity_webgl_games_setup_post_types');
+  // Top-Level Menus (https://developer.wordpress.org/plugins/administration-menus/top-level-menus/)
+  add_action('admin_menu', 'unity_webgl_games_admin_page');
+}
+
+main();
