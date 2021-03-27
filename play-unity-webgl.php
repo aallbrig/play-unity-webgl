3e49ff90465de428798454924d1fec4ddecdb6d0
Andrew Allbright <aallbrig@gmail.com>
2021-03-27 12:14:34 -0400

    Initial plugin file added

---
+++ play-unity-webgl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Play Unity WebGL
+ * Plugin URI: https://github.com/aallbrig/play-unity-webgl
+ * Description: Upload your Unity WebGL builds and play through them on your wordpress site!
+ * Version: v0.0.0
+ * Author: Andrew Allbright
+ * Author URI: https://www.andrewallbright.com
+*/
+?>
